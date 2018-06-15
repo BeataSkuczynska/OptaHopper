f316377fd5e1f199fc9f63bc9b794a1cbe63f013
Zuchens <paulina.zak1@gmail.com>
2018-06-15 16:59:26 -0700

    added scripts for running sentiment analysis

---
+++ README.md
@@ -0,0 +1,17 @@
+Download OPFI - Opinion Finder - a hybrid (CRF+patterns) application for extracting opinion targets in Polish:
+http://zil.ipipan.waw.pl/OPTA
+
+Download Sentiment dictionary:
+http://zil.ipipan.waw.pl/SlownikWydzwieku
+
+For sentiment:
+
+Download and build fastText from source
+example:
+cd treehopper
+git clone https://github.com/facebookresearch/fastText.git
+make fastText
+
+echo "Install pytorch"
+
+pip install http://download.pytorch.org/whl/cu80/torch-0.1.12.post2-cp35-cp35m-linux_x86_64.whl
--- treehopper_using.sh
+++ treehopper_using.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
+
 if [ ! -d trehopper ]; then
     echo " Downloading trehopper repository"
     git clone https://github.com/Zuchens/treehopper
 fi
-if [ ! -d OPTA ]; then
-    echo " Downloading OPTA"
- #   wget http://zil.ipipan.waw.pl/OPTA?action=AttachFile&do=get&target=opta-tagger.tar.gz
-#    tar -xzf OPTA-patterns-0.1.tar.gz
+
+cd treehopper
+WITH_TRAIN=$1
+SENTIMENT_DICTIONARY=SlownikWydzwieku01.csv
+
+./fetch_data.sh
+pip3 install -r requirements.txt
+cd treehopper
+export PYTHONPATH=$(pwd)
+
+if [ ${WITH_TRAIN} == 'train']; then
+    train.py --dictionaries ${SENTIMENT_DICTIONARY}
 fi
-if [ ! -d out-szczerosci ]; then
-    mkdir out-szczerosci
-fi
-# download Korpus Szczerosci
+
+python predict.py --model_path model.pth \
+               --input_parents test/polevaltest_parents.txt \
+               --input_sentences test/polevaltest_sentence.txt \
+               --output ../../resources/treehopper_sentiment.txt
+
+cd ../..
+export PYTHONPATH=$(pwd)
+
+
+
 
 python prepare_conll_for_treehopper.py resources/opta_test.conll resources/
 python treehopper/treehopper/predict.py --model_path treehopper/model_1.pth --input_parents resources/parents.txt --input_sentences resources/sentences.txt --output resources/treehopper_sentiment.txt
