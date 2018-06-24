f9f831ed424b3430a94bc74e72ba3ce446e4069a
Beata <b.skuczynska@student.uw.edu.pl>
2018-06-25 01:09:38 +0200

    Bugfix

--- prepare_data.sh
+++ prepare_data.sh
@@ -9,8 +9,8 @@ USER=$4
 
 sudo python3 -m pip install -r requirements.txt
 
-if [ ! -d resources/predict ]; then
-    mkdir resources/predict
+if [ ! -d resources ]; then
+    mkdir resources resources/predict
 fi
 
 
